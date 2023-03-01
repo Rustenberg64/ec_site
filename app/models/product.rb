d1fc8f3c246799b11c05ce1029286a4cbb14d2a9
noname <your.email@example.com>
2023-03-02 01:20:14 +0900

    Introduce Active Storage

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :manufacturer
+    validates :price
+    validates :description
+    validates :stock
+    validates :image
+  end
+  has_one_attached :image
 end
