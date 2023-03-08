9e9cb01075bc54dfb8d07be1c54993724097f1ad
noname <your.email@example.com>
2023-03-08 22:52:23 +0900

    Create dropdown menu for sort

--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -1,7 +1,17 @@
 class Customer::ProductsController < ApplicationController
 
   def index
-    @pagy, @products = pagy(Product.with_attached_image, items: 2)
+    products = Product.with_attached_image
+    if sort_param == "price_low_to_high"
+      sort_products = products.order(:price)
+    elsif sort_param == "price_high_to_low"
+      sort_products = products.order(price: :DESC)
+    elsif sort_param == "date_old_to_new"
+      sort_products = products.order(:created_at)
+    else
+      sort_products = products.order(created_at: :DESC)
+    end
+    @pagy, @products = pagy(sort_products, items: 12)
   end
 
   def show
@@ -9,5 +19,11 @@ class Customer::ProductsController < ApplicationController
     @cart_item = @product.cart_items.build
     @cart_item.quantity = 1
   end
+
+  private
+
+    def sort_param
+      params[:sort]
+    end
 
 end
