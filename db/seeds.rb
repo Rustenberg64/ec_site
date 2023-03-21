d41210c1a158d2772819bd08a897a4fe276657e7
noname <your.email@example.com>
2023-03-22 00:51:31 +0900

    Modify created_at column for product in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,112 +8,171 @@ Customer.create(
   password: "password"
 )
 
+# 16
+product = Product.new(
+  name: "カッター",
+  manufacturer: Faker::Company.name,
+  price: 580,
+  description: "オレンジ色のカッター",
+  stock: rand(50),
+  created_at: Time.now,
+  updated_at: Time.now
+)
+product.image.attach(io: File.open("./db/fixtures/bunbougu_cutter.png"), filename: 'bunbougu_cutter.png')
+product.save
+
+# 10
+product = Product.new(
+  name: "鉛筆",
+  manufacturer: Faker::Company.name,
+  price: 90,
+  description: "緑色の一般的な鉛筆",
+  stock: rand(50),
+  created_at: Time.now.ago(10.hours),
+  updated_at: Time.now.ago(10.hours)
+)
+product.image.attach(io: File.open("./db/fixtures/bunbougu_enpitsu.png"), filename: 'bunbougu_enpitsu.png')
+product.save
+
+datetime_from = Time.now.ago(20.years)
+datetime_to   = Time.now.ago(1.day)
+
 # 1
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "水筆",
   manufacturer: Faker::Company.name,
   price: 1080,
   description: "水彩画や水筆書道などをに使われる中に水を内蔵することができる筆、水筆（ウォーターブラシ）",
   stock: rand(50),
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/bunbougu_mizufude.png"), filename: 'bunbougu_mizufude.png')
 product.save
 
 # 2
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "ボールペン",
   manufacturer: Faker::Company.name,
   price: 110,
   description: "シンプルな透明のボールペン",
   stock: rand(50),
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/bunbougu_ballpen.png"), filename: 'bunbougu_ballpen.png')
 product.save
 
 # 3
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "製図ペン",
   manufacturer: Faker::Company.name,
   price: 820,
   description: "図面を書くときに便利なペン（シャーペン）",
   stock: rand(50),
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/seizu_pen.png"), filename: 'seizu_pen.png')
 product.save
 
 # 4
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "筆ペン",
   manufacturer: Faker::Company.name,
   price: 320,
   description: "先が筆のようになっていて毛筆のような字を書くことができるペン",
   stock: rand(50),
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/bunbougu_fudepen.png"), filename: 'bunbougu_fudepen.png')
 product.save
 
 # 5
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "定規",
   manufacturer: Faker::Company.name,
   price: 320,
   description: "青い定規",
   stock: rand(50),
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/bunbougu_jougi.png"), filename: 'bunbougu_jougi.png')
 product.save
 
 # 6
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "万年筆",
   manufacturer: Faker::Company.name,
   price: 10500,
   description: "インクを補充して繰り返し使える、黒い万年筆",
   stock: rand(50),
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/pen_mannenhitsu.png"), filename: 'pen_mannenhitsu.png')
 product.save
 
 # 7
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "消しゴム",
   manufacturer: Faker::Company.name,
   price: 80,
   description: "青いカバーのついた消しゴム",
   stock: rand(50),
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/bunbougu_keshigomu.png"), filename: 'bunbougu_keshigomu.png')
 product.save
 
 # out of stock
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "左ハンドル車用のマジックハンド",
   manufacturer: Faker::Company.name,
   price: 2000,
   description: "自動車の窓の外に手が届かないときに使われるマジックハンド",
   stock: 0,
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/car_magichand.png"), filename: 'car_magichand.png')
 product.save
 
 # 8
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "カラフルなマスキングテープ",
   manufacturer: Faker::Company.name,
   price: 350,
   description: "カラフルな柄が印刷されたかわいいマスキングテープ",
   stock: rand(50),
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/bunbougu_masking_tape.png"), filename: 'bunbougu_masking_tape.png')
 product.save
 
 # 9
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "穴あけパンチ",
   manufacturer: Faker::Company.name,
   price: 640,
   description: "青い穴あけパンチ",
   stock: rand(50),
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/bunbougu_anaake_punch.png"), filename: 'bunbougu_anaake_punch.png')
 product.save
@@ -130,80 +189,71 @@ product.save
 # product.save
 
 # 11
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "修正テープ",
   manufacturer: Faker::Company.name,
   price: 380,
   description: "テープ状の修正液を使って消しゴムのように文字を消せる修正テープ",
   stock: rand(50),
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/bunbougu_syusei_tape.png"), filename: 'bunbougu_syusei_tape.png')
 product.save
 
 # 12
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "ガムテープ",
   manufacturer: Faker::Company.name,
   price: 480,
   description: "シンプルなガムテープ",
   stock: rand(50),
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/tape_gumtape.png"), filename: 'tape_gumtape.png')
 product.save
 
 # 13
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "ハサミ",
   manufacturer: Faker::Company.name,
   price: 580,
   description: "水色のハサミ",
   stock: rand(50),
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/bunbougu_hasami.png"), filename: 'bunbougu_hasami.png')
 product.save
 
 # 14
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "グラスリッツェン",
   manufacturer: Faker::Company.name,
   price: 2500,
   description: "ガラスに模様を彫刻するときに使われる道具、グラスリッツェン",
   stock: rand(50),
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/dougu_glasritzen_.png"), filename: 'dougu_glasritzen_.png')
 product.save
 
 # 15
+date = rand(datetime_from..datetime_to)
 product = Product.new(
   name: "スティックのり",
   manufacturer: Faker::Company.name,
   price: 370,
   description: "水色のラインが入ったスティックのり",
   stock: rand(50),
-  created_at: Time.now.prev_year
+  created_at: date,
+  updated_at: date
 )
 product.image.attach(io: File.open("./db/fixtures/bunbougu_sticknori.png"), filename: 'bunbougu_sticknori.png')
-product.save
-
-# 16
-product = Product.new(
-  name: "カッター",
-  manufacturer: Faker::Company.name,
-  price: 580,
-  description: "オレンジ色のカッター",
-  stock: rand(50),
-  created_at: Time.now.next_year
-)
-product.image.attach(io: File.open("./db/fixtures/bunbougu_cutter.png"), filename: 'bunbougu_cutter.png')
-product.save
-
-# 10
-product = Product.new(
-  name: "鉛筆",
-  manufacturer: Faker::Company.name,
-  price: 90,
-  description: "緑色の一般的な鉛筆",
-  stock: rand(50),
-)
-product.image.attach(io: File.open("./db/fixtures/bunbougu_enpitsu.png"), filename: 'bunbougu_enpitsu.png')
 product.save
