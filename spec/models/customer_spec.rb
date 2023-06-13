d9a0f7f5e078921c1aec68fc3c0bcaa08c333e27
noname <your.email@example.com>
2023-06-13 23:54:53 +0900

    Change setting not to use ssl

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Customer, type: :model do
+  # メール、パスワードがあれば有効な状態であること
+  it "is valid with a email, and password" do
+    customer = Customer.new(
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze"
+    )
+    expect(customer).to be_valid
+  end
+  # 名がなければ無効な状態であること
+  it "is invalid without a first name"
+  # 姓がなければ無効な状態であること
+  it "is invalid without a last name"
+  # メールアドレスがなければ無効な状態であること
+  it "is invalid without an email address"
+  # 重複したメールアドレスなら無効な状態であること
+  it "is invalid with a duplicate email address" do
+    Customer.create(
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze"
+    )
+    customer = Customer.new(
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze"
+    )
+    customer.valid?
+    expect(customer.errors[:email]).to include("has already been taken")
+  end
+  # ユーザーのフルネームを⽂字列として返すこと
+  it "returns a user's full name as a string"
+end
